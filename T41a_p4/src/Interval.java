15a77713401f05204193f52c455a37de4f48e7c9
Tanner <tanwolf67@gmail.com>
2017-04-10 14:09:58 -0500

    Interval, Interval node work

--- T41a_p4/src/Interval.java
+++ T41a_p4/src/Interval.java
@@ -2,39 +2,66 @@
 public class Interval<T extends Comparable<T>> implements IntervalADT<T> {
 
     // TODO declare any needed data members
+	private T start;
+	private T end;
+	private String label;
 
     public Interval(T start, T end, String label) {
-        // TODO Auto-generated constructor stub
+        // TODO check if end < start??
+    	if (end.compareTo(start) < 0) {
+    		throw new IllegalArgumentException();
+    	}
+    	
+    	this.start = start;
+    	this.end = end;
+    	this.label = label;
     }
 
     @Override
     public T getStart() {
-        // TODO Auto-generated method stub
+    	return start;
     }
 
     @Override
     public T getEnd() {
-        // TODO Auto-generated method stub
+    	return end;
     }
 
     @Override
     public String getLabel() {
-        // TODO Auto-generated method stub
+    	return label;
     }
 
     @Override
     public boolean overlaps(IntervalADT<T> other) {
         // TODO Auto-generated method stub
+    	
+    	
+    	
+    	return true;
     }
 
     @Override
     public boolean contains(T point) {
-        // TODO Auto-generated method stub
+        // check if the point is greater than or equal to the start and less than or equal to the end
+    	return (start.compareTo(point) <= 0 && point.compareTo(end) <= 0 );
     }
 
     @Override
     public int compareTo(IntervalADT<T> other) {
-        // TODO Auto-generated method stub
+        // check if start times are the same
+    	if (start.compareTo(other.getStart()) == 0) {
+    		// if they are, return the comparison of the ends
+    		return end.compareTo(other.getEnd());
+    	}
+    	// if starts not the same, return comparison of starts
+    	return start.compareTo(other.getStart());
     }
 
+    @Override
+	public String toString() {
+    	// return String form in form of: label [start, end]
+    	return label + " [" + start.toString() + ", " + end.toString() + "]";
+    }
+    
 }
--- T41a_p4/src/IntervalTree.java
+++ T41a_p4/src/IntervalTree.java
@@ -1,19 +1,28 @@
+import java.util.LinkedList;
 import java.util.List;
 
 public class IntervalTree<T extends Comparable<T>> implements IntervalTreeADT<T> {
 	
 	// TODO declare any data members needed for this class
+	private int size = 0;					// not sure if we need these but they might be helpful - T
+	private IntervalNode<T> root = null; 		// 
 
 	@Override
 	public IntervalNode<T> getRoot() {
 		// TODO Auto-generated method stub
-		return null;
+		return root;
 	}
 
 	@Override
 	public void insert(IntervalADT<T> interval)
 					throws IllegalArgumentException {
-		// TODO Auto-generated method stub
+		// TODO Recursively insert
+		if (interval == null) {
+			throw new IllegalArgumentException();
+		}
+		if (size == 0) {
+			root = new IntervalNode<T>(interval);
+		}
 
 	}
 
@@ -29,32 +38,71 @@ public class IntervalTree<T extends Comparable<T>> implements IntervalTreeADT<T>
 					IntervalADT<T> interval)
 					throws IntervalNotFoundException, IllegalArgumentException {
 		// TODO Auto-generated method stub
+		
+		
+		return node;
 	}
 
 	@Override
 	public List<IntervalADT<T>> findOverlapping(
 					IntervalADT<T> interval) {
 		// TODO Auto-generated method stub
+		
+		
+		return null;
 	}
 
 	@Override
 	public List<IntervalADT<T>> searchPoint(T point) {
 		// TODO Auto-generated method stub
+		List<IntervalADT<T>> list = new LinkedList<IntervalADT<T>>();
+		// Iterate through tree
+		
+		// Check if interval contains point
+		
+		return list;
 	}
 
 	@Override
 	public int getSize() {
 		// TODO Auto-generated method stub
+		return size;
 	}
 
 	@Override
 	public int getHeight() {
 		// TODO Auto-generated method stub
+		// recursively count?
+		return getHeight(root);
+	}
+	
+	/**
+	 * Helper method to find height of tree
+	 * 
+	 * @param node
+	 * @return max height of tree
+	 */
+	private int getHeight(IntervalNode<T> node) {
+		int leftH = 1;
+		int rightH = 1;
+		
+		// Recursion to find height
+		if (node.getLeftNode() != null)
+			leftH = 1 + getHeight(node.getLeftNode());
+		if (node.getRightNode() == null)
+			rightH = 1 + getHeight(node.getRightNode());
+		
+		// return max: if one is null, height remains 1
+		if (leftH > rightH) {
+			return leftH;
+		}
+		return rightH;
 	}
 
 	@Override
 	public boolean contains(IntervalADT<T> interval) {
 		// TODO Auto-generated method stub
+		return false;
 	}
 
 	@Override
