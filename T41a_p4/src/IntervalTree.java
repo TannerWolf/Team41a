0a2131e1c7c5a19ca4e21fe92c81e2529844c78f
JiayueLai <jlai28@wisc.edu>
2017-04-12 22:10:57 -0500

    Add files via upload

--- T41a_p4/src/IntervalTree.java
+++ T41a_p4/src/IntervalTree.java
@@ -10,7 +10,7 @@ public class IntervalTree<T extends Comparable<T>> implements IntervalTreeADT<T>
 		root = this.getRoot();
 		return root;
 	}
-    public IntervalNode<T> search(IntervalNode<T> root, T left, T right){
+    /**public IntervalNode<T> search(IntervalNode<T> root, T left, T right){
     	//Check to see whether is reaching the end
     	//Do we need more check?????
         if (root == null)
@@ -30,21 +30,59 @@ public class IntervalTree<T extends Comparable<T>> implements IntervalTreeADT<T>
             
     
     }
+    **/
 	@Override
 	public void insert(IntervalADT<T> interval)
 					throws IllegalArgumentException {
 		if (interval == null)
 			throw new IllegalArgumentException();
+		
+		insert(root, interval);
+		
+		
+	}
+	
+	public IntervalNode<T> insert(IntervalNode<T> T, IntervalADT<T> interval){
+		//If the tree is empty,create a new one
 		if (root == null) {
 			root = new IntervalNode<T>(interval);
+			return root;
+		}//If interval's start is different and is smaller, add to left sub tree
+		//recursively trace 
+		if (interval.getStart().compareTo(root.getInterval().getStart()) < 0){
+			root.setLeftNode(insert(root.getLeftNode(), interval));
+			
 		}
-				
+		//Insert it into T's right subtree if it's start is 
+		//different and larger than that of T
+		if ((interval.getStart().compareTo(T.getInterval().getStart()) > 0)){
+			root.setRightNode(insert(root.getLeftNode(), interval));	
+		}
+		
+		//If interval's start is equal to that of T, than compare its end
+		else if (interval.getEnd().compareTo(T.getInterval().getEnd()) < 0){
+			root.setLeftNode(insert(root.getLeftNode(), interval));
+		}
+		//Insert it into T's right subtree if it's start is 
+		//different and larger than that of T
+		else if ((interval.getEnd().compareTo(T.getInterval().getEnd()) > 0)){
+			root.setRightNode(insert(root.getLeftNode(), interval));
+		}
+		//return the unchanged node pointer
+		return root;
+		
 	}
 
 	@Override
 	public void delete(IntervalADT<T> interval)
 					throws IntervalNotFoundException, IllegalArgumentException {
-		// TODO Auto-generated method stub
+		if (interval == null) 
+			throw new IllegalArgumentException();
+		//not sure
+		if (!interval.overlaps(interval))
+			throw new IntervalNotFoundException(interval.toString());
+			
+		deleteHelper(root,interval);
 
 	}
 
