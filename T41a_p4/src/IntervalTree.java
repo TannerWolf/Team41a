f6a0720effc64d5ab831d2c20c74480b215204cf
Crilliam <willmustari@gmail.com>
2017-04-13 22:01:45 -0500

    Delete works, need updateMaxEnd

--- T41a_p4/src/IntervalTree.java
+++ T41a_p4/src/IntervalTree.java
@@ -15,18 +15,7 @@ import java.util.List;
 
 public class IntervalTree<T extends Comparable<T>> implements IntervalTreeADT<T> {
 	
-	private IntervalNode<T> root;
-
-	/*
-	 * Constructor
-	 */
-	public IntervalTree(IntervalNode<T> root){
-		this.root = root;
-	}
-	
-	public IntervalTree() {
-		root = null;
-	}
+	private IntervalNode<T> root = null;
 
 	@Override
 	public IntervalNode<T> getRoot() {
@@ -41,14 +30,12 @@ public class IntervalTree<T extends Comparable<T>> implements IntervalTreeADT<T>
 		
 		//Calls helper method
 		root = insert(root, interval);
+		// TODO call recalculateMaxEnd here??
 	}
 	
 	private IntervalNode<T> insert(IntervalNode<T> node, IntervalADT<T> interval){
 		//If the tree is empty, create a new node
-		//update maxEnd??
-		
 		if (node == null) {
-			
 			node = new IntervalNode<T>(interval);
 			return node;
 		}
@@ -63,28 +50,27 @@ public class IntervalTree<T extends Comparable<T>> implements IntervalTreeADT<T>
 			node.setRightNode(insert(node.getRightNode(), interval));	
 		}
 		
-		
-		
 		//Return the unchanged node pointer
 		return node;
 	}
+
 
 	@Override
 	public void delete(IntervalADT<T> interval)
 					throws IntervalNotFoundException, IllegalArgumentException {
 		if (interval==null)
 			throw new IllegalArgumentException();
+		if (contains(interval) == false)
+			throw new IntervalNotFoundException(interval.getLabel()+" ["+interval.getStart()+", "+interval.getEnd()+"]");
+		//Calls helper method
 		deleteHelper(root, interval);
-		
-
+		// TODO call recalculateMaxEnd here?
 	}
 
 	@Override
 	public IntervalNode<T> deleteHelper(IntervalNode<T> node,
-					IntervalADT<T> interval)
-					throws IntervalNotFoundException, IllegalArgumentException {
-		if (interval == null)
-			throw new IntervalNotFoundException(interval.toString());
+					IntervalADT<T> interval){
+		// TODO Insert comments
 		if (interval.compareTo(node.getInterval()) == 0){
 			if (node.getLeftNode()==null && node.getRightNode()==null){
 				return null;
@@ -105,7 +91,6 @@ public class IntervalTree<T extends Comparable<T>> implements IntervalTreeADT<T>
 		else
 			node.setRightNode(deleteHelper(node.getRightNode(), interval));
 		return node;
-		
 	}
 
 	 private T recalculateMaxEnd(IntervalNode<T> node){
@@ -114,10 +99,8 @@ public class IntervalTree<T extends Comparable<T>> implements IntervalTreeADT<T>
 		
 		 else //if (node.getMaxEnd().compareTo(node.getRightNode().getMaxEnd()) < 0)
 			 return node.getLeftNode().getMaxEnd();
-		
-		
 	 }
-	 
+
 	@Override
 	public List<IntervalADT<T>> findOverlapping(
 					IntervalADT<T> interval) {
@@ -213,28 +196,22 @@ public class IntervalTree<T extends Comparable<T>> implements IntervalTreeADT<T>
 	}
 	
 	private boolean containsHelper(IntervalNode<T> node, IntervalADT<T> interval){
-		// TODO FINISH HELPER METHOD > CODE BELOW MAY HELP
-		return false;
+	    if (node == null) {
+	        return false;
+	    }
+	    // if interval equals current node's interval, returns true
+	    if (interval.compareTo(node.getInterval()) == 0) {
+	        return true;
+	    }
+	    // interval < this node's interval; look in left subtree
+	    if (interval.compareTo(node.getInterval()) < 0) {
+	        return containsHelper(node.getLeftNode(), interval);
+	    }
+	    // interval > this node's interval; look in left subtree
+	    else {
+	        return containsHelper(node.getRightNode(), interval);
+	    }
 	}
-	
-//  public IntervalNode<T> search(IntervalNode<T> root, T left, T right){
-//	//Check to see whether is reaching the end
-//	//Do we need more check?????
-//    if (root == null)
-//    	return root;
-//    // Compare to find whether is greater than root's start
-//    //If 'start' are same, compare the 'end' values.
-//    if (root.getInterval().getStart().compareTo(left) != 0){
-//    	 if (root.getInterval().getStart().compareTo(left) > 0)
-//             return search( root.getLeftNode(), left, right);
-// 		return search(root.getRightNode(), left, right);
-//    }
-//    //The situation that start are the same
-//    if (root.getInterval().getEnd().compareTo(right) > 0){
-//    	return search( root.getLeftNode(), left, right);
-//    }
-//	return search(root.getRightNode(), left, right);
-//}
 
 	@Override
 	public void printStats() {
